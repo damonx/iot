43fe72c5b551efebda3769f51868f4d8057fa1f6
Damon Xu <akldamonx@icloud.com>
2018-08-27 23:38:33 +1200

    Dockerized the application via lb

--- usercenter-boot/src/main/java/com/fpa/usercenter/UsercenterBootApplication.java
+++ usercenter-boot/src/main/java/com/fpa/usercenter/UsercenterBootApplication.java
@@ -2,11 +2,20 @@ package com.fpa.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class UsercenterBootApplication {
+@ComponentScan(basePackageClasses = UplusCloudController.class)
+public class UsercenterBootApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(UsercenterBootApplication.class);
+	}
+
+	public static void main(final String[] args) {
 		SpringApplication.run(UsercenterBootApplication.class, args);
 	}
 }
